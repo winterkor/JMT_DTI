5f5d9af318d416558cc93d14ae5df1d6d32bd77f
winterkor <winterforfunacc@gmail.com>
2025-04-13 23:59:38 +0800

    clean up

--- test/Data_creation.py
+++ test/Data_creation.py
@@ -7,7 +7,7 @@ async def main():
     print("Scanning for nearby tools...")
     devices = await BleakScanner.discover(timeout=20.0)
 
-    with open("Integration/tool_database.txt", "w") as f:
+    with open("test/tool_database.txt", "w") as f:
         for d in devices:
             if d.name and d.name.startswith("Tool_"):
                 print(f"{d.name} - {d.address}")
